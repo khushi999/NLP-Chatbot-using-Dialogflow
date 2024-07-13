d0f3b9fa123234c1281ef902f09696dfb7503d05
Khushi Jain <83177168+khushi999@users.noreply.github.com>
2024-07-13 18:03:19 +0530

    Update generic_helper.py
    Added comments

--- generic_helper.py
+++ generic_helper.py
@@ -1,7 +1,6 @@
-# Author: Dhaval Patel. Codebasics YouTube Channel
-
 import re
 
+# Converts the dict input into string format
 def get_str_from_food_dict(food_dict: dict):
     result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
     return result
